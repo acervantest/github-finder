373a84982170f7df129953d8da5b4596317a42e9
alejandro <acervantest83@gmail.com>
2021-04-29 21:39:16 -0700

    http request implementation

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,35 +3,16 @@ import UserItem from './UserItem';
 
 class Users extends Component {
 
-    state = {
-        users: [
-            {
-                id:'2',
-                login: 'defunkt',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v4',
-                html_url: 'https://github.com/defunkt'
-            },
-            {
-                id:'3',
-                login: 'defunkt',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v4',
-                html_url: 'https://github.com/defunkt'
-            },
-            {
-                id:'4',
-                login: 'defunkt',
-                avatar_url: 'https://avatars0.githubusercontent.com/u/2?v4',
-                html_url: 'https://github.com/defunkt'
-            }
-        ]
-    }
-
     render() {
         return (  
             <div style={userStyle}>
-            { this.state.users.map( user => (
-                    <UserItem key={user.id} user={user} />
-            ))}
+            { 
+                this.props.loading ? <div> loading... </div> :
+
+                this.props.users.map( user => (
+                        <UserItem key={user.id} user={user} />
+                ))
+            }
             </div>   
         )
     }
