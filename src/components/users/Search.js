71656792f276312cbde69ca58bcafa46b5861195
alejandro <acervantest83@gmail.com>
2021-04-30 23:58:25 -0700

    clear button component implementation

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,7 +8,9 @@ class Search extends Component {
     }
 
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -20,6 +22,8 @@ class Search extends Component {
     }
 
     render() {
+
+        const { showClear, clearUsers } = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit} className='form'>
@@ -28,6 +32,10 @@ class Search extends Component {
                         placeholder='Search users' />
                     <input type='submit' value='search' className='btn btn-dark btn-block' />
                 </form>
+                { showClear && 
+                    ( <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button> )
+                }
+                
             </div>
         )
     }
