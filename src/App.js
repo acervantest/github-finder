a372a1e70c29c2194c6ee0eb017be5e1af0e5ba7
alejandro <acervantest83@gmail.com>
2021-04-30 23:34:33 -0700

    search component implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -21,11 +22,22 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  searchUsers =  async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false });
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar title="Github Finder" icon="fab fa-github"/>
         <div className='container'>
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users} />
         </div> 
       </Fragment >
