90422575328e2237db39c0ed2c3e3c7175e96193
alejandro <acervantest83@gmail.com>
2021-05-02 22:43:39 -0700

    home component implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
+import Home from './components/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -20,12 +19,7 @@ const App = () => {
       <div className='container'>
         <Alert />
         <Switch>
-          <Route exact path='/' render={ () => (
-            <Fragment>
-              <Search />
-              <Users />
-            </Fragment>
-          )}/>
+          <Route exact path='/' component={Home}/>
           <Route path='/about' component={About} />
           <Route path='/user/:login' component={User} />
         </Switch> 
