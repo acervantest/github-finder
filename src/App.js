685f8d3ae7666624026112801c4a2ea898de6c19
alejandro <acervantest83@gmail.com>
2021-05-02 23:14:44 -0700

    not found component implementation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import User from './components/users/User';
 import Home from './components/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About} />
           <Route path='/user/:login' component={User} />
+          <Route component={NotFound} />
         </Switch> 
       </div> 
     </Fragment>
